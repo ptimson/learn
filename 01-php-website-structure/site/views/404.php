e5b9ad76e125ffe65b4403d0e532064098a0d0ce
Peter Timson <p@timson.me>
2017-02-20 20:13:33 +0000

    Added 404 page

--- 01-php-website-structure/site/views/404.php
+++ 01-php-website-structure/site/views/404.php
@@ -1,1 +1,14 @@
-<h1>404</h1>
+<?php
+include('header.php');
+?>
+
+<div class="container">
+
+  <div class="starter-template">
+    <h1>404</h1>
+  </div>
+
+</div><!-- /.container -->
+
+<?php
+include('footer.php');
